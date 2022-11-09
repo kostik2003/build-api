431175469ac14587b2c63dfb83e84d8754279905
kostya <kostya.ivanchik@mail.ru>
2022-11-09 12:38:16 +0300

    instantiating_PC

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,23 +1,20 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-import { Product } from 'schemas/products.schema';
 import { ProductsService } from './products.service';
-
-const prisma = new PrismaClient();
+import { prisma } from 'prisma/prisma';
 
 @Controller('products')
 export class ProductsController {
     constructor(readonly productsService: ProductsService) {}
 
-    // @Get() // test request for postgres
-    // async getHello() {
-    //     let posts;
-    //     posts = await prisma.post.findMany();
-    //     return posts;
-    // }
-
-    @Get() //test request for mongo
-    getAll(): Promise<Product[]> {
-        return this.productsService.getAll();
+    @Get() // test request for postgres
+    async getHello() {
+        let posts;
+        posts = await prisma.post.findMany();
+        return posts;
     }
+
+    // @Get() //test request for mongo
+    // getAll(): Promise<Product[]> {
+    //     return this.productsService.getAll();
+    // }
 }

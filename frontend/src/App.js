97d7c907288c2cceaa5fe2504265074217fb76c1
kostya <kostya.ivanchik@mail.ru>
2022-12-20 15:15:57 +0300

    create navigate to privatePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,13 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { privateRoutes, publicRoutes } from "./routes";
-import { Context } from ".";
 import { observer } from "mobx-react-lite";
-import AuthPage from "./pages/authPage";
-import RegisterPage from "./pages/registerPage";
-import TrackingPage from "./pages/trackingPage";
-import { AUTH_ROUTE } from "./utils/consts";
+import AppRouter from "./component/AppRouter";
 
 const App = () => {
-  const { store } = useContext(Context);
-  useEffect(() => {
-    if (localStorage.getItem("token")) {
-      store.checkAuth();
-    }
-  }, []);
-
   return (
     <BrowserRouter>
-      <Routes>
-        {publicRoutes.map(({ path, Component }) => (
-          <Route
-            key={path}
-            path={path}
-            element={<Component />} //разлогинивает и редиректит на одну и ту же страницу.
-            exact
-          />
-        ))}
-        {store.isAuth &&
-          privateRoutes.map(({ path, Component }) => (
-            <Route key={path} path={path} element={<Component />} exact />
-          ))}
-        <Route path="*" element={<Navigate to={AUTH_ROUTE} replace />} />
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   );
 };
---
+++ frontend/src/component/AppRouter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
+import { privateRoutes, publicRoutes } from "../routes";
+import { Context } from "..";
+import { observer } from "mobx-react-lite";
+import { AUTH_ROUTE } from "../utils/consts";
+
+const AppRouter = () => {
+  const { store } = useContext(Context);
+
+  return (
+    <Routes>
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} exact />
+      ))}
+      {store.isAuth &&
+        privateRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} exact />
+        ))}
+      <Route path="*" element={<Navigate to={AUTH_ROUTE} replace />} />
+    </Routes>
+  );
+};
+export default observer(AppRouter);
